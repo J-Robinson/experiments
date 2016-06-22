d8362756eae3639c4650f9770e1a23e33e9add47
Jonathan Robinson <jonathan@therobinsons.net.nz>
2016-06-22 10:46:22 +1000

    Added text

--- open_cv_face_detect/ObjectDetection.cpp
+++ open_cv_face_detect/ObjectDetection.cpp
@@ -12,15 +12,22 @@ using namespace cv;
 void detectAndDisplay( Mat frame );
 
 /** Global variables */
-String face_cascade_name = "./haarcascade_frontalface_alt.xml";
-//String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
-String eyes_cascade_name = "./haarcascade_eye.xml";
+//String face_cascade_name = "./haarcascade_frontalface_alt.xml";
+//String eyes_cascade_name = "./haarcascade_eye.xml";
+//String nose_cascade_name = "./haarcascade_mcs_nose.xml";
 
-String nose_cascade_name = "./haarcascade_mcs_nose.xml";
+String face_cascade_name = "./haarcascade_frontalface_alt.xml";
+//String eyes_cascade_name = "./haarcascade_eyes_45.xml";
+String both_eyes_cascade_name = "./haarcascade_both_eyes.xml";
+String left_eye_cascade_name = "./haarcascade_left_eye.xml";
+String right_eye_cascade_name = "./haarcascade_right_eye.xml";
+String nose_cascade_name = "./haarcascade_nose_25.xml";
 
 CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
+CascadeClassifier both_eyes_cascade;
 CascadeClassifier nose_cascade;
+CascadeClassifier left_eye_cascade;
+CascadeClassifier right_eye_cascade;
 
 string window_name = "CDO Data Science - Face detection";
 
@@ -40,12 +47,24 @@ int main( int argc, const char** argv )
         return -1; 
     }
 
-    if( !eyes_cascade.load( eyes_cascade_name ) )
+    if( !both_eyes_cascade.load( both_eyes_cascade_name ) )
     { 
-        printf("--(!)Error loading eyes cascade\n"); 
+        printf("--(!)Error loading both eyes cascade\n"); 
         return -1; 
     }
 
+    if( !left_eye_cascade.load( left_eye_cascade_name ) )
+    { 
+        printf("--(!)Error loading left eye cascade\n"); 
+        return -1; 
+    }
+    
+    if( !right_eye_cascade.load( right_eye_cascade_name ) )
+    { 
+        printf("--(!)Error loading right eye cascade\n"); 
+        return -1; 
+    }
+    
     if (!nose_cascade.load( nose_cascade_name ))
     {
         printf("--(!)Error loading nose cascade\n");
@@ -94,29 +113,51 @@ void detectAndDisplay( Mat frame )
         Mat faceROI = frame_gray( faces[i] );
         std::vector<Rect> eyes;
     
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 4, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+        //-- In each face, detect both eyes
+        both_eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
         
-        std::size_t numEyes = std::min(static_cast<std::size_t>(2), eyes.size());
+        std::size_t numEyes = std::min(static_cast<std::size_t>(1), eyes.size());
        
         for( size_t j = 0; j < numEyes; j++ )
         {
             Point eyeCenter( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-            ellipse( frame, eyeCenter, Size( eyes[i].width*0.5, eyes[i].height*0.2), 0, 0, 360, Scalar( 255, 255, 0 ), 4, 8, 0 );
+            ellipse( frame, eyeCenter, Size( eyes[j].width*0.5, eyes[j].height*0.3), 0, 0, 360, Scalar( 255, 255, 0 ), 4, 8, 0 );
             //int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
             //circle( frame, center, radius, Scalar( 255, 255, 0 ), 4, 8, 0 );
         }
 
         //-- Detect nose
         std::vector<Rect> nose;
-        nose_cascade.detectMultiScale( faceROI, nose, 1.2, 4, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+        nose_cascade.detectMultiScale( faceROI, nose, 1.2, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
         if (nose.size() > 0)
         {
             Point noseCenter( faces[i].x + nose[0].x + nose[0].width*0.5, faces[i].y + nose[0].y + nose[0].height*0.5 );
             ellipse( frame, noseCenter, Size( nose[0].width*0.2, nose[0].height*0.5), 0, 0, 360, Scalar( 0, 255, 255 ), 4, 8, 0 );
         }
+       
+        //-- Detect left eye
+        std::vector<Rect> leftEye;
+        left_eye_cascade.detectMultiScale( faceROI, leftEye, 1.2, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+        //if (leftEye.size() > 0)
+        for (size_t j=0 ; j < leftEye.size(); ++j)
+        {
+            Point leftEyeCenter( faces[i].x + leftEye[j].x + leftEye[j].width*0.5, faces[i].y + leftEye[j].y + leftEye[j].height*0.5 );
+            ellipse( frame, leftEyeCenter, Size( leftEye[j].width*0.5, leftEye[j].height*0.5), 0, 0, 360, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+        
+        //-- Detect right eye
+        std::vector<Rect> rightEye;
+        left_eye_cascade.detectMultiScale( faceROI, rightEye, 1.2, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+        //if (rightEye.size() > 0)
+        for (size_t j=0 ; j < rightEye.size(); ++j)
+        {
+            Point rightEyeCenter( faces[i].x + rightEye[j].x + rightEye[j].width*0.5, faces[i].y + rightEye[j].y + rightEye[j].height*0.5 );
+            ellipse( frame, rightEyeCenter, Size( rightEye[j].width*0.5, rightEye[j].height*0.5), 0, 0, 360, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
     }
+
     putText(frame, "CDO Data Science", Point(10, frame.size().height-10), FONT_HERSHEY_PLAIN, 4.0 /*scale*/, CV_RGB(255,255,255), 4.0 /*lineType*/, LINE_AA);
+
     //-- Show what you got
     //namedWindow(window_name, CV_WINDOW_NORMAL);
     //setWindowProperty(window_name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
